1201ef30ce9cdff8d915f1a24369206e396b9ff0
cba_tony <tony@cybridge.jp>
2025-03-07 13:24:00 +0900

    Update alias

--- alias/alias.sh
+++ alias/alias.sh
@@ -16,7 +16,7 @@ alias v='less'
 alias vi='vim'
 alias pf='vi ~/.zshrc'
 alias pfb='vi ~/.bashrc'
-alias rlb='source ~/.bashrc; source ~/.zshrc; echo "Reloaded bashrc and zshrc"'
+alias rlpf='source ~/.zshrc; echo "Reloaded .zshrc"'
 
 # Goto
 alias ch='cd /etc/httpd/vhosts'
@@ -46,7 +46,7 @@ mssh() { if [[ $@ == "" ]]; then make bash; else; make bash1 n="$@"; fi }
 mreup() { if [[ $@ == "" ]]; then make reup; else; make reup1 n="$@"; fi }
 mdown() { if [[ $@ == "" ]]; then make down; else; make down1 n="$@"; fi }
 
-alias dot-update="~/.dotfiles/bootstrap.sh true && source ~/.zshrc && source ~/.bashrc"
+alias dot-update="~/.dotfiles/bootstrap.sh true && source ~/.zshrc"
 alias dot-menu="~/.dotfiles/bootstrap.sh"
 alias dot-uninstall="~/.dotfiles/uninstall.sh"
 #alias debug="echo 1"
